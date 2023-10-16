942f1167a458768e9a7b41edb8918bd6f5b52fa4
Phil <souzaherodp@gmail.com>
2023-10-15 20:02:40 -0400

    remove small change

--- src/components/homePage/index.js
+++ src/components/homePage/index.js
@@ -12,7 +12,7 @@ function HomePage(props) {
                 <div className="bio">
                     <p>
                         an avid developer who loves using STEM to make art and
-                        help other people. OOf this page needs to change
+                        help other people.
                     </p>
                     <br />
                     <p>
