e6f1fd092b5fd8092e27aa92295f4dcec14e3a42
Phil <souzaherodp@gmail.com>
2023-10-17 21:17:20 -0400

    tes tes ttest

--- src/components/homePage/index.js
+++ src/components/homePage/index.js
@@ -12,7 +12,7 @@ function HomePage(props) {
                 <div className="bio">
                     <p>
                         an avid developer who loves using STEM to make art and
-                        help other people.
+                        help other people. test test test
                     </p>
                     <br />
                     <p>
